0da6e14a69a7dac83f6ffa867898967511e707f0
Dan Newman <dnewms@umich.edu>
2026-01-27 15:09:21 -0500

    fix: remove extra whitespace in short dropdown lists

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -35,18 +35,16 @@ const SelectScrollUpButton = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.ScrollUpButton>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollUpButton>
 >(({ className, ...props }, ref) => (
-  <div className="flex items-center justify-center py-1 h-6">
-    <SelectPrimitive.ScrollUpButton
-      ref={ref}
-      className={cn(
-        "flex cursor-default items-center justify-center",
-        className
-      )}
-      {...props}
-    >
-      <ChevronUpIcon className="h-4 w-4" />
-    </SelectPrimitive.ScrollUpButton>
-  </div>
+  <SelectPrimitive.ScrollUpButton
+    ref={ref}
+    className={cn(
+      "flex cursor-default items-center justify-center py-1",
+      className
+    )}
+    {...props}
+  >
+    <ChevronUpIcon className="h-4 w-4" />
+  </SelectPrimitive.ScrollUpButton>
 ))
 SelectScrollUpButton.displayName = SelectPrimitive.ScrollUpButton.displayName
 
@@ -54,18 +52,16 @@ const SelectScrollDownButton = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.ScrollDownButton>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollDownButton>
 >(({ className, ...props }, ref) => (
-  <div className="flex items-center justify-center py-1 h-6">
-    <SelectPrimitive.ScrollDownButton
-      ref={ref}
-      className={cn(
-        "flex cursor-default items-center justify-center",
-        className
-      )}
-      {...props}
-    >
-      <ChevronDownIcon className="h-4 w-4" />
-    </SelectPrimitive.ScrollDownButton>
-  </div>
+  <SelectPrimitive.ScrollDownButton
+    ref={ref}
+    className={cn(
+      "flex cursor-default items-center justify-center py-1",
+      className
+    )}
+    {...props}
+  >
+    <ChevronDownIcon className="h-4 w-4" />
+  </SelectPrimitive.ScrollDownButton>
 ))
 SelectScrollDownButton.displayName =
   SelectPrimitive.ScrollDownButton.displayName
